c3f5ff569b480d0aabb50108cc8ba70921d2bd8d
DefyCab <love.bellinder@gmail.com>
2021-11-24 18:01:23 +0100

    request from Open Weather add temperature on screen

--- src/component/WeatherReport.jsx
+++ src/component/WeatherReport.jsx
@@ -3,28 +3,46 @@ import axios from "axios";
 
 const WeatherReport = () => {
   const [geolocation, setGeolocation] = useState({});
-  const [weatherInfo, setWeatherInfo] = useState({ city: '', temperature: '' });
+  const [weatherInfo, setWeatherInfo] = useState({ city: "", temp: "" });
 
   const fetchUserCoordinates = () => {
     return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy: true });
+      navigator.geolocation.getCurrentPosition(resolve, reject, {
+        enableHighAccuracy: true,
+      });
     });
   };
 
   const fetchGeolocation = async () => {
     const position = await fetchUserCoordinates();
-    const openCageResponse = await axios.get('https://api.opencagedata.com/geocode/v1/json',
+    const openCageResponse = await axios.get(
+      "https://api.opencagedata.com/geocode/v1/json",
       {
         params: {
           key: "0d816e86fbf44c508de125ee0f992c22",
-          q: `${position.coords.latitude}+${position.coords.longitude}`
-        }
-      });
+          q: `${position.coords.latitude}+${position.coords.longitude}`,
+        },
+      }
+    );
+    const openWeatherResponse = await axios.get(
+      "https://api.openweathermap.org/data/2.5/onecall",
+      {
+        params: {
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+          exclude: "minutely",
+          units: "metric",
+          appid: "3c28e383c661b24a09ef856aceeba64a"
+        },
+      }
+    );
     setGeolocation(position.coords);
     setWeatherInfo({
+      temp: openWeatherResponse.data.current.temp,
       city: openCageResponse.data.results[0].components.city
-    })
+    });
   };
+  
 
   useEffect(() => {
     fetchGeolocation();
@@ -32,6 +50,7 @@ const WeatherReport = () => {
 
   return (
     <div>
+      <input type="text" value={weatherInfo.temp} />
       <input type="text" value={geolocation.latitude} />
       <input type="text" value={geolocation.longitude} />
       <input type="text" value={weatherInfo.city} />
