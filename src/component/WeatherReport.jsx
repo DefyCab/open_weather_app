bf3b600fa4cf9e8d762774a33eb5ee38d5f20909
giacoletti <giacoletti92@gmail.com>
2021-11-24 17:20:50 +0100

    Adding axios get request to opencage API to fetch city from user coordinates displaying city on screen

--- src/component/WeatherReport.jsx
+++ src/component/WeatherReport.jsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const WeatherReport = () => {
   const [geolocation, setGeolocation] = useState({});
+  const [weatherInfo, setWeatherInfo] = useState({ city: '', temperature: '' });
 
   const fetchUserCoordinates = () => {
-    // navigator.geolocation.getCurrentPosition(position => { setGeolocation(position.coords) });
     return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
+      navigator.geolocation.getCurrentPosition(resolve, reject, { enableHighAccuracy: true });
     });
   };
 
+  const fetchGeolocation = async () => {
+    const position = await fetchUserCoordinates();
+    const openCageResponse = await axios.get('https://api.opencagedata.com/geocode/v1/json',
+      {
+        params: {
+          key: "0d816e86fbf44c508de125ee0f992c22",
+          q: `${position.coords.latitude}+${position.coords.longitude}`
+        }
+      });
+    setGeolocation(position.coords);
+    setWeatherInfo({
+      city: openCageResponse.data.results[0].components.city
+    })
+  };
+
   useEffect(() => {
-    async fetchAnyData() => {
-      const position = await fetchUserCoordinates();
-      console.log(position)
-    };
+    fetchGeolocation();
   }, []);
 
   return (
     <div>
       <input type="text" value={geolocation.latitude} />
       <input type="text" value={geolocation.longitude} />
+      <input type="text" value={weatherInfo.city} />
     </div>
   );
 };
