478523ae9389004ff3825bddd7d2c0904ec483b1
DefyCab <love.bellinder@gmail.com>
2021-11-25 15:34:00 +0100

    hide api keys for safety reasons since in a public repo on github

--- src/component/WeatherReport.jsx
+++ src/component/WeatherReport.jsx
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const WeatherReport = () => {
-  const [geolocation, setGeolocation] = useState({});
-  const [weatherInfo, setWeatherInfo] = useState({ city: "", temp: "" });
+  const [geolocation, setGeolocation] = useState({})
+  const [weatherInfo, setWeatherInfo] = useState({ city: '', temp: '' })
 
   const fetchUserCoordinates = () => {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject, {
         enableHighAccuracy: true,
-      });
-    });
-  };
+      })
+    })
+  }
 
   const fetchGeolocation = async () => {
-    const position = await fetchUserCoordinates();
+    const position = await fetchUserCoordinates()
     const openCageResponse = await axios.get(
-      "https://api.opencagedata.com/geocode/v1/json",
+      'https://api.opencagedata.com/geocode/v1/json',
       {
         params: {
-          key: "0d816e86fbf44c508de125ee0f992c22",
+          key: config.OPEN_CAGE_APPID,
           q: `${position.coords.latitude}+${position.coords.longitude}`,
         },
-      }
-    );
+      },
+    )
     const openWeatherResponse = await axios.get(
-      "https://api.openweathermap.org/data/2.5/onecall",
+      'https://api.openweathermap.org/data/2.5/onecall',
       {
         params: {
           lat: position.coords.latitude,
           lon: position.coords.longitude,
-          exclude: "minutely",
-          units: "metric",
-          appid: "3c28e383c661b24a09ef856aceeba64a"
+          exclude: 'minutely',
+          units: 'metric',
+          appid: config.OPEN_WEATHER_APPID,
         },
-      }
-    );
-    setGeolocation(position.coords);
+      },
+    )
+    setGeolocation(position.coords)
     setWeatherInfo({
       temp: openWeatherResponse.data.current.temp,
-      city: openCageResponse.data.results[0].components.city
-    });
-  };
-  
+      city: openCageResponse.data.results[0].components.city,
+    })
+  }
 
   useEffect(() => {
-    fetchGeolocation();
-  }, []);
+    fetchGeolocation()
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <div>
@@ -55,7 +55,7 @@ const WeatherReport = () => {
       <input type="text" value={geolocation.longitude} />
       <input type="text" value={weatherInfo.city} />
     </div>
-  );
-};
+  )
+}
 
-export default WeatherReport;
+export default WeatherReport
